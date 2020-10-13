86c65ebc670ad2c8385a566d5b0c4e1edbae0e87
jose angel linarez <ydauracontreras@gmail.com>
2020-10-13 00:37:22 -0300

    Nuevos cambios

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const server = require('./api/server');
 
-const HOST = 'localhost'
-const PORT = 4000;
 
-
-server.listen(PORT, () => console.log(`Server Running at ${HOST}:${PORT}`))
+server.set('port', (process.env.PORT || 5000))
