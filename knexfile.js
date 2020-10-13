d2288c19fbc1a5ff0960b56155059eb27a574e56
jose angel linarez <ydauracontreras@gmail.com>
2020-10-13 09:24:23 -0300

    tidi

--- knexfile.js
+++ knexfile.js
@@ -17,16 +17,13 @@ module.exports = {
     },
   },
   production:{
-    client: 'pg',
-    connection: {
-      filename:  process.env.DATABASE_URL,
-    },
-    migrations:{
-      directory:'./db/migrations'
-    },
-    seeds:{
-      directory: './db/seeds'
-    }
+      client: 'pg',
+      connection: process.env.DATABASE_URL,
+      migrations: {
+          directory: __dirname + '/db/migrations',
+        },
+      seeds: {
+          directory: __dirname + '/db/seeds',
   }
-
+  }
 }
