624ee93660643b9c0eb7cbddee47dac155ad6fa1
jose angel linarez <ydauracontreras@gmail.com>
2020-10-15 11:21:58 -0300

    todo list api developed with node.js

--- server.js
+++ server.js
@@ -94,9 +94,6 @@ const {id} = req.params
 app.put('/todos/:id/status' , async (req, res) =>{
     const id = req.params.id
     const status = req.body.status
-    if(!status){
-        return res.status(400).json({message: 'You must include a status in your request'})
-    }
         try{     
       await knex('todos').where({id: id}).update({status: status})
             res.status(200).json({message:"the Status was succesfully updated"})
